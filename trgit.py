e228c18af52fa551db57404a8297c5be44a78b57
afaq09 <64742861+afaq09@users.noreply.github.com>
2020-05-14 13:56:05 +0500

    Update and rename trgat.py to trgit.py

--- trgit.py
+++ trgit.py
@@ -20,18 +20,31 @@ try:
 except ModuleNotFoundError:
     print '[!] Module >Mechanize< Not Found!\n    This module is only available in python 2.x :/\n    Please install mechanize (pip install mechanize) and run the program with python2'
     exit()
-Print"""
-\033[1;94m╱╱╱╱╱╱╱╱╭╮
-\033[1;96m╱╱╱╱╱╱╱╱┃┃
-\033[1;98m╭╮╭╮╭┳━━┫┃╭━━┳━━┳╮╭┳━━╮
-\033[1;96m┃╰╯╰╯┃┃━┫┃┃╭━┫╭╮┃╰╯┃┃━┫
-\033[1;92m╰╮╭╮╭┫┃━┫╰┫╰━┫╰╯┃┃┃┃┃━┫
-\033[1;96m╱╰╯╰╯╰━━┻━┻━━┻━━┻┻┻┻━━╯"""
-Print
+
 time.sleep(0.5)
-user = raw_input('[?] \033[1;96m Target Username/ID/Email >>> ')
+#Dev:love_hacker
+##### LOGO #####
+logo = """
+\033[1;96m╱╱╱╱╱╱╱╱╭╮
+\033[1;96m╱╱╱╱╱╱╱╱┃┃
+\033[1;96m╭╮╭╮╭┳━━┫┃╭━━┳━━┳╮╭┳━━╮
+\033[1;96m┃╰╯╰╯┃┃━┫┃┃╭━┫╭╮┃╰╯┃┃━┫
+\033[1;96m╰╮╭╮╭┫┃━┫╰┫╰━┫╰╯┃┃┃┃┃━┫
+\033[1;96m╱╰╯╰╯╰━━┻━┻━━┻━━┻┻┻┻━━╯
+  \033[1;96m::♧♧♧♧♧♧♧♧♧♧\033[1;91mWhatsapp    \033[1;96m....0 3 1 0 0 2 8 5 7 9 0 ..... \033[1;96m♧♧♧♧♧♧♧♧♧♧▒▒▒▒▒▒▒::::        
+  \033[1;91m:》》》\033[1;93mDon't use it for Illegal activities ....\033[1;91m《《《▒▒▒▒▒▒▒▒▒▒▒:::::
+\033[1;95m♡╭───•◈•────╮♡\033[1;96m-▒▒▒ZUBAIR▒▒▒▒▒-\033[1;95m♡╭───•,◈•────╮♡
+\033[1;95m ________  _____  _____  ______        _       _____  _______      
+\033[1;95m|  __   _||_   _||_   _||_   _ \      / \     |_   _||_   __ \     
+\033[1;95m|_/  / /    | |    | |    | |_) |    / _ \      | |    | |__) |    
+\033[1;95m   .'.' _   | '    ' |    |  __'.   / ___ \     | |    |  __ /     
+\033[1;95m _/ /__/ |   \ \__/ /    _| |__) |_/ /   \ \_  _| |_  _| |  \ \_   
+\033[1;95m|________|    `.__.'    |_______/|____| |____||_____||____| |___|                                                                  
+\033[1;95m♡╰───•◈•───╯♡\033[1;96mZUBAIR Ahmad Hacker\033[1;95m♡╰───•◈•───╯♡"""
+
+user = raw_input('[?] Target Username/ID/Email >>> ')
 time.sleep(0.8)
-wrdlstFileName = raw_input('\n[?] \033[1;96mEnter Your Wordlist Directory >>> ')
+wrdlstFileName = raw_input('\n[?] Wordlist Directory >>> ')
 try:
     wordlist = open(wrdlstFileName, 'r')
 except FileNotFoundError:
@@ -39,17 +52,10 @@ except FileNotFoundError:
     exit()
 
 time.sleep(0.8)
-print '\033[1;96m\n\nCracking '+user+' Now...'
+print '\n\nCracking '+user+' Now...'
 
 time.sleep(1)
-print '
-\033[1;95m ________  _____  _____  ______        _       _____  _______      
-\033[1;95m|  __   _||_   _||_   _||_   _ \      / \     |_   _||_   __ \     
-\033[1;95m|_/  / /    | |    | |    | |_) |    / _ \      | |    | |__) |    
-\033[1;95m   .'.' _   | '    ' |    |  __'.   / ___ \     | |    |  __ /     
-\033[1;95m _/ /__/ |   \ \__/ /    _| |__) |_/ /   \ \_  _| |_  _| |  \ \_   
-\033[1;95m|________|    `.__.'    |_______/|____| |____||_____||____| |___|  
-                                                                   '
+print '\n##############√zubair Ahmad TargetAttack√#############\n'
 for password in wordlist:
     if password == '' or password == ' ':
         pass
