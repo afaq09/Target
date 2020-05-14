9c5d40d3b3989d28c0e511c43dcb33e9556aa8b4
afaq09 <64742861+afaq09@users.noreply.github.com>
2020-05-14 20:31:50 +0500

    Update and rename fbgt.py to fbtarget.py

--- fbtarget.py
+++ fbtarget.py
@@ -21,28 +21,8 @@ except ModuleNotFoundError:
     print '[!] Module >Mechanize< Not Found!\n    This module is only available in python 2.x :/\n    Please install mechanize (pip install mechanize) and run the program with python2'
     exit()
 
-#Dev:love_hacker
-##### LOGO #####
-Print """
-\033[1;96m╱╱╱╱╱╱╱╱╭╮
-\033[1;96m╱╱╱╱╱╱╱╱┃┃
-\033[1;96m╭╮╭╮╭┳━━┫┃╭━━┳━━┳╮╭┳━━╮
-\033[1;96m┃╰╯╰╯┃┃━┫┃┃╭━┫╭╮┃╰╯┃┃━┫
-\033[1;96m╰╮╭╮╭┫┃━┫╰┫╰━┫╰╯┃┃┃┃┃━┫
-\033[1;96m╱╰╯╰╯╰━━┻━┻━━┻━━┻┻┻┻━━╯
-  \033[1;96m::♧♧♧♧♧♧♧♧♧♧\033[1;91mWhatsapp    \033[1;96m....0 3 1 0 0 2 8 5 7 9 0 ..... \033[1;96m♧♧♧♧♧♧♧♧♧♧▒▒▒▒▒▒▒::::        
-  \033[1;91m:》》》\033[1;93mDon't use it for Illegal activities ....\033[1;91m《《《▒▒▒▒▒▒▒▒▒▒▒:::::
-\033[1;95m♡╭───•◈•────╮♡\033[1;96m-▒▒▒ZUBAIR▒▒▒▒▒-\033[1;95m♡╭───•,◈•────╮♡
-\033[1;95m ________  _____  _____  ______        _       _____  _______      
-\033[1;95m|  __   _||_   _||_   _||_   _ \      / \     |_   _||_   __ \     
-\033[1;95m|_/  / /    | |    | |    | |_) |    / _ \      | |    | |__) |    
-\033[1;95m   .'.' _   | '    ' |    |  __'.   / ___ \     | |    |  __ /     
-\033[1;95m _/ /__/ |   \ \__/ /    _| |__) |_/ /   \ \_  _| |_  _| |  \ \_   
-\033[1;95m|________|    `.__.'    |_______/|____| |____||_____||____| |___|
-\033[1;93m                                                    
-
 time.sleep(0.5)
-user = raw_input('[?] Target Username/ID/Email >>> ')
+user = raw_input('[?]Enter Your Target Username/ID/Email >>> ')
 time.sleep(0.8)
 wrdlstFileName = raw_input('\n[?] Wordlist Directory >>> ')
 try:
@@ -55,7 +35,7 @@ time.sleep(0.8)
 print '\n\nCracking '+user+' Now...'
 
 time.sleep(1)
-print '\n##############√zubair Ahmad TargetAttack√#############\n'
+print '\n##############√AFAQ AHMAD TargetAttack√#############\n'
 for password in wordlist:
     if password == '' or password == ' ':
         pass
